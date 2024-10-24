27c0d3be84eb54f9681d903918fda35021aaf0a9
Akshaya Muthukumar <akshaya1907@gmail.com>
2024-10-24 16:27:38 +0530

    api_url changed

--- src/config.js
+++ src/config.js
@@ -1,11 +1,4 @@
 const API_URL = 'https://mydresscode-be.onrender.com/api';
-<<<<<<< HEAD
-
+//Oct24
 export default API_URL;
 
-
-
-=======
-
-export default API_URL;
->>>>>>> e6841558a4ae97404a1b85b9a768cd5409cd6972
